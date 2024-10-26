2013add093d60f70c6eb60f38eebafdf3c4fb2af
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-10-27 00:21:34 +0100

    Update responsiveness and animation

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 const burger = $(".burger-bar");
 const navItems = $(".nav-items");
 const navUl = $(".nav-ul");
+const skillDesktop = $(".skills-desktop");
+const skillDesktopList = $(".skills-desktop").children();
+const skillMobile = $(".skills-mobile").children();
+const skillMobileList = $(".skills-mobile").children();
+const options = {
+    threshold: 0.5,
+    rootmargin: "-500px",
+};
 
 //Custom cursor movement
 document.body.onmousemove = function(e) {
     if (e.pageX >= window.innerWidth - 90) {
-      console.log("Cursor is at the right edge of the screen");
+      $("#cursor").css({
+        left: e.pageX - 90,
+        top: e.pageY - 90
+      });
     }
     else{
       $("#cursor").css({
@@ -20,6 +31,24 @@ burger.click(
     function(){
       navItems.toggleClass("nav-open");
       navUl.toggleClass("nav-ul-display");
-    }  
-  );
-  
+    }
+);
+
+skillDesktopList.css("opacity", "0");
+skillMobileList.css("opacity", "0");
+const observerOne = new IntersectionObserver(function (entries, observer) {
+    if (entries[0].isIntersecting === true) {
+      skillDesktopList.css("opacity", "1");
+      skillDesktopList.css("animation", "slidein 2s");
+    }
+}, options);
+
+const observerTwo = new IntersectionObserver(function (entries, observer) {
+   if (entries[0].isIntersecting === true) {
+     skillMobileList.css("opacity", "1");
+     skillMobileList.css("animation", "slidein 2s");
+   }
+}, options);
+
+observerOne.observe(skillDesktop[0]);
+observerTwo.observe(skillMobile[0]);
